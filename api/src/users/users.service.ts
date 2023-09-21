b7799ed9f401ca64961b6ef066b1e55aefd802ed
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-09-21 19:12:53 -0300

    criar recursos do usuario

---
+++ api/src/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor() {}
+
+  create(createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+}
