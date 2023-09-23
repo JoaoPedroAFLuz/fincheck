240cd57af2cc178f37e8c2933c4e439e7881f5ef
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-09-22 21:05:24 -0300

    criar rota de autenticacao e refactor no criar usuario

--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -1,57 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { hash } from 'bcryptjs';
+import { Injectable } from '@nestjs/common';
 
 import { UsersRepository } from '@/shared/database/repositories/users.repositories';
-import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
-
-  async create(createUserDto: CreateUserDto) {
-    const { firstName, lastName, email, password } = createUserDto;
-
-    const emailAlreadyInUse = await this.usersRepository.findByEmail(email);
-
-    if (emailAlreadyInUse) {
-      throw new ConflictException('Email already in use');
-    }
-
-    const hashedPassword = await hash(password, 10);
-
-    const user = await this.usersRepository.create({
-      data: {
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-        categories: {
-          createMany: {
-            data: [
-              // Income
-              { name: 'Salário', icon: 'salary', type: 'INCOME' },
-              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
-              { name: 'Outro', icon: 'other', type: 'INCOME' },
-              // Expense
-              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
-              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
-              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
-              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
-              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
-              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
-              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
-              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
-              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
-            ],
-          },
-        },
-      },
-    });
-
-    return {
-      id: user.id,
-      name: user.firstName + ' ' + user.lastName,
-      email: user.email,
-    };
-  }
 }
