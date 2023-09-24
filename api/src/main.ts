13c19263fca097b1c7b7a28aac4c7d3ad451155a
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-09-24 11:54:22 -0300

    habilitar cors para todas as origens

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,6 +7,9 @@ import { env } from './shared/config/env';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
 
   await app.listen(env.port || 3000);
 }
