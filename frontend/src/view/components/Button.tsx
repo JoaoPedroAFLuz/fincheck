078bb3c4f56faf307250e2cff5e97409ebc82807
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-09-25 22:47:20 -0300

    frontend - componente do botao

---
+++ frontend/src/view/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps } from 'react';
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function Button(props: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className="h-12 w-full rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400"
+    ></button>
+  );
+}
+
