ab4a59cd504db2c630bcb276b9581404a11ec07f
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-10-07 10:13:28 -0300

    frontend - adicionar dropdown menu no menu do usuario

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,10 +1,30 @@
+import { useAuth } from '@/app/hooks/useAuth';
+import { ExitIcon } from '@radix-ui/react-icons';
+import { DropdownMenu } from './DropdownMenu';
+
 export function UserMenu() {
+  const { logout } = useAuth();
+
   return (
-    <div className="flex h-12 w-12 items-center justify-center rounded-full bg-teal-50">
-      <span className="text-sm font-medium tracking-tight text-teal-900">
-        JL
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-teal-50">
+          <span className="text-sm font-medium tracking-tight text-teal-900">
+            JL
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32">
+        <DropdownMenu.Item
+          onSelect={logout}
+          className="flex items-center justify-between gap-2"
+        >
+          <span>Sair</span>
+          <ExitIcon className="h-4 w-4" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
 
