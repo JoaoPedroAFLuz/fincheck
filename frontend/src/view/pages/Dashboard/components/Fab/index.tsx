3b3a881a29a0249a67fc6c32a1e0f06b67ce6e00
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-10-27 21:46:37 -0300

    frontend - ajustar z-index do FAB

--- frontend/src/view/pages/Dashboard/components/Fab/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Fab/index.tsx
@@ -12,7 +12,7 @@ export function Fab() {
     <div className="fixed bottom-4 right-4">
       <DropdownMenu.Root>
         <DropdownMenu.Trigger>
-          <button className="flex h-12 w-12 items-center justify-center rounded-full bg-teal-900 text-white">
+          <button className="z-50 flex h-12 w-12 items-center justify-center rounded-full bg-teal-900 text-white">
             <PlusIcon className="h-6 w-6" />
           </button>
         </DropdownMenu.Trigger>
